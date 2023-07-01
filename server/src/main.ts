07a65f9d1acc8b32ebd74ee1c07d1367c38a7fce
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-07-01 00:18:26 -0400

    swagger api title changed

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Hotel Management System')
+    .setDescription('The HMS API description')
     .setVersion('1.0')
     .addTag('user')
     .addTag('staff')
@@ -27,7 +27,7 @@ async function bootstrap() {
     .addTag('booking')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/swagger', app, document);
 
   await app.listen(3001);
 }
