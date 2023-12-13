40cf1ed505f57f691af92da316f8e791e8b6528a
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2023-12-13 19:47:43 -0400

    tremor-label removed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,7 +104,6 @@ const config: Config = {
         "tremor-full": "9999px",
       },
       fontSize: {
-        "tremor-label": ["0.75rem"],
         "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
         "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
         "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
