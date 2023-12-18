7164cc2df6859fabaf01d0923f260dd69023e932
73009242+adeirjunior@users.noreply.github.com <73009242+adeirjunior@users.noreply.github.com>
2023-12-18 16:20:07 -0400

    created the src folder temp

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -1,20 +1,21 @@
 import SideNav from "@/_ui/components/dashboard/sidenav";
 import DashboardHeader from "@/_ui/components/header/DashboardHeader";
+import "@/_ui/styles/globals.css"
 
 export default function LandingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>
+  return <div className="h-screen">
     <DashboardHeader />
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow py-6 md:overflow-y-auto md:py-12">
+      <div className="flex-grow w-full h-full p-6 md:overflow-y-auto md:py-12 md:border-1 md:rounded-lg md:border-gray-200">
         {children}
       </div>
     </div>
-  </>
+  </div>
 }
